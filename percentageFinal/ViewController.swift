d0544290691ff39616c1e0221660570fddae8a5c
Swapnil Beta <swapnildhanwal@hotmail.com>
2016-10-03 08:40:24 +0530

    Initial Commit

---
+++ percentageFinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  percentageFinal
+//
+//  Created by Swapnil Dhanwal on 03/10/16.
+//  Copyright © 2016 Swapnil Dhanwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
